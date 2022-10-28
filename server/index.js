7b59e10ec4f562d0a51d1c5c4e459f4f475f2207
BABUNAIK BANOTH <104496776+Babu5banoth@users.noreply.github.com>
2022-10-28 15:38:04 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,57 @@
+
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors=require("cors")
+
+
+
+
+
+const port = process.env.PORT || 7000
+
+//DB connection
+
+require("../server/db/connections")
+
+
+
+
+//required routes
+
+const userRoutes = require("./routes/employeeroutes")
+const adminRoutes = require("./routes/employerroutes")
+const jobPost = require("./routes/jobpostroutes")
+const jobHistory = require("./routes/jobHistoryroutes")
+
+
+
+
+
+//middleware
+
+app.use(express.json())
+app.use(cors())
+
+
+
+//routes
+
+app.use("/api/user",userRoutes);
+app.use("/api/admin",adminRoutes )
+app.use("/api/jobpost",jobPost)
+app.use("/api/jobhistory",jobHistory)
+
+
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("hello babu")
+})
+
+
+app.listen(port, () => {
+    console.log(`serveris running at port : ${port}`)
+})
